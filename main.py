4b10fd6cdf4df85c95a7fddfd98594f04893e711
rheadsouza03 <97720582+rheadsouza03@users.noreply.github.com>
2024-05-28 16:36:32 +1200

    Completed main.nearest_neighbours_heuristic() and utility.calculate_total_distance(). Additionally, I have added in the indexes to for each node so that they can be viewed in the plot, and have begun the main.savings_heuristic(). Finally, I added in a utility.is_total_capacity_valid() to make the computation for the savings heuristic a little easier.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import numpy as np
+
 import utility as utility
 import loader as loader
 
 
 def main():
-
     # Paths to the data and solution files.
     vrp_file = "data/n32-k5.vrp"  # "data/n80-k10.vrp"
     sol_file = "data/n32-k5.sol"  # "data/n80-k10.sol"
@@ -15,27 +16,24 @@ def main():
     vrp_best_sol = loader.load_solution(sol_file)
     best_distance = utility.calculate_total_distance(vrp_best_sol, px, py, depot)
     print("Best VRP Distance:", best_distance)
-    utility.visualise_solution(vrp_best_sol, px, py, depot, "Optimal Solution")
+    utility.visualise_solution(vrp_best_sol, px, py, depot, "Optimal Solution: " + sol_file)
 
     # Executing and visualizing the nearest neighbour VRP heuristic.
-    # Uncomment it to do your assignment!
-
     # nnh_solution = nearest_neighbour_heuristic(px, py, demand, capacity, depot)
     # nnh_distance = utility.calculate_total_distance(nnh_solution, px, py, depot)
     # print("Nearest Neighbour VRP Heuristic Distance:", nnh_distance)
-    # utility.visualise_solution(nnh_solution, px, py, depot, "Nearest Neighbour Heuristic")
+    # utility.visualise_solution(nnh_solution, px, py, depot, "Nearest Neighbour Heuristic: "+vrp_file)
 
     # Executing and visualizing the saving VRP heuristic.
     # Uncomment it to do your assignment!
-    
-    # sh_solution = savings_heuristic(px, py, demand, capacity, depot)
-    # sh_distance = utility.calculate_total_distance(sh_solution, px, py, depot)
-    # print("Saving VRP Heuristic Distance:", sh_distance)
-    # utility.visualise_solution(sh_solution, px, py, depot, "Savings Heuristic")
+
+    sh_solution = savings_heuristic(px, py, demand, capacity, depot)
+    sh_distance = utility.calculate_total_distance(sh_solution, px, py, depot)
+    print("Saving VRP Heuristic Distance:", sh_distance)
+    utility.visualise_solution(sh_solution, px, py, depot, "Savings Heuristic")
 
 
 def nearest_neighbour_heuristic(px, py, demand, capacity, depot):
-
     """
     Algorithm for the nearest neighbour heuristic to generate VRP solutions.
 
@@ -47,13 +45,50 @@ def nearest_neighbour_heuristic(px, py, demand, capacity, depot):
     :return: List of vehicle routes (tours).
     """
 
-    # TODO - Implement the Nearest Neighbour Heuristic to generate VRP solutions.
+    # Initialise the route with the depot
+    unvisited_indices = set(range(len(px)))
+    unvisited_indices.remove(depot)
+    routes = []
 
-    return None
+    current_route = []
+    current_load = 0
+    current_index = depot
+
+    while len(unvisited_indices) != 0:
+        # Search for a valid nearest-neighbour
+        nearest_neighbour = (-1, np.inf)
+        for i in unvisited_indices:
+            distance = utility.calculate_euclidean_distance(px, py, current_index, i)
+
+            if (distance < nearest_neighbour[1]) & (capacity >= (current_load + demand[i])):
+                nearest_neighbour = (i, distance)
+
+        # Add the new nearest neighbour to the route
+        if nearest_neighbour[0] > -1:
+            current_index = nearest_neighbour[0]
+            current_route.append(current_index)
+            unvisited_indices.remove(current_index)
+            current_load += demand[current_index]
+
+        # Reached capacity or no valid neighbours found, finish the current route
+        if ((capacity == current_load) | (nearest_neighbour[0] == -1) |
+                ((capacity == current_load) & (nearest_neighbour[0] == -1))):
+            # Finish the route
+            routes.append(current_route)
+
+            # Re-initialise the variables
+            current_route = []
+            current_index = depot
+            current_load = 0
+
+    # Append the depot to complete the last route if it's not empty
+    if current_route:
+        routes.append(current_route)
+
+    return routes
 
 
 def savings_heuristic(px, py, demand, capacity, depot):
-
     """
     Algorithm for Implementing the savings heuristic to generate VRP solutions.
 
@@ -66,8 +101,23 @@ def savings_heuristic(px, py, demand, capacity, depot):
     """
 
     # TODO - Implement the Saving Heuristic to generate VRP solutions.
+    # Initialise the route with the depot
+    routes = [[i] for i in range(len(px))]
+    routes.remove([depot])
+    savings = {}
 
-    return None
+    # Compute the savings for each possible merge
+    for i in range(len(routes) - 1):
+        distance = (utility.calculate_euclidean_distance(px, py, routes[i][0], depot) +
+                    utility.calculate_euclidean_distance(px, py, depot, routes[i + 1][0]) -
+                    utility.calculate_euclidean_distance(px, py, routes[i][0], routes[i + 1][0]))
+
+        savings[(routes[i][0], routes[i + 1][0])] = distance
+    print(savings)
+
+
+
+    return routes
 
 
 if __name__ == '__main__':
--- utility.py
+++ utility.py
@@ -16,7 +16,6 @@ def calculate_euclidean_distance(px, py, index1, index2):
     :return: Euclidean distance between node 1 and 2.
     """
 
-    # TODO - Implement the euclidean distance function.
     x_diff = px[index1] - px[index2]
     y_diff = py[index1] - py[index2]
 
@@ -28,24 +27,52 @@ def calculate_euclidean_distance(px, py, index1, index2):
 def calculate_total_distance(routes, px, py, depot):
 
     """
-    Calculating the total Euclidean distance of a solution.
+    Calculating the total Euclidean distance of all routes in a solution.
 
     :param routes: List of routes (list of lists).
     :param px: List of X coordinates for each node.
     :param py: List of Y coordinates for each node.
     :param depot: Depot.
-    :return: Total tour euclidean distance.
+    :return: Total tour Euclidean distance.
     """
+    total_distance = 0
+    for route in routes:
+        # Add the distance from the depot back to the first node to the total distance
+        total_distance += calculate_euclidean_distance(px, py, depot, route[0])
 
-    # TODO - Implement function for finding the total euclidean distance of the learned tour.
+        # Add the distance between customer nodes to the total distance
+        for i in range(len(route) - 1):
+            total_distance += calculate_euclidean_distance(px, py, route[i], route[i + 1])
 
-    return None
+        # Add the distance from the last node back to the depot to the total distance
+        total_distance += calculate_euclidean_distance(px, py, depot, route[-1])
+
+    return total_distance
+
+
+def is_total_capacity_valid(routes, demand, capacity):
+    """
+    Checking if the total demand across all routes are valid, for savings heuristic.
+
+    :param routes: list of routes (list of lists).
+    :param demand: list of each node's demand (list of numbers).
+    :param capacity: total capacity allowed across the both routes to be merged.
+    :return: True is capacity >= total demand. Otherwise, false.
+    """
+    total_load = 0
+    for route in routes:
+        for node_index in route:
+            total_load += demand[node_index]
+
+            if total_load > capacity:
+                return False
+
+    return True
 
 
 def visualise_solution(vrp_sol, px, py, depot, title):
-
     """
-    Function for visualise the tour on a 2D figure.
+    Function to visualize the tour on a 2D figure.
 
     :param vrp_sol: The vrp solution, which is a list of lists (excluding the depot).
     :param px: List of X coordinates for each node.
@@ -53,11 +80,9 @@ def visualise_solution(vrp_sol, px, py, depot, title):
     :param depot: the depot index
     :param title: Plot title.
     """
-
     n_routes = len(vrp_sol)
-    s_vrp_sol = vrp_sol
 
-    # Set axis too slightly larger than the set of x and y
+    # Set axis to slightly larger than the set of x and y
     min_x, max_x, min_y, max_y = min(px), max(px), min(py), max(py)
     center_x = (min_x + max_x) / 2
     center_y = (min_y + max_y) / 2
@@ -74,21 +99,24 @@ def visualise_solution(vrp_sol, px, py, depot, title):
     plt.ylim(min_y, max_y)
 
     plt.plot(px[depot], py[depot], 'rs', markersize=5)
+    plt.text(px[depot], py[depot], str(depot), fontsize=12, ha='right')
 
     cmap = plt.cm.get_cmap("tab20", n_routes)
     for k in range(n_routes):
-        a_route = s_vrp_sol[k]
+        a_route = vrp_sol[k]
 
         # Draw the route: linking to the depot
         plt.plot([px[depot], px[a_route[0]]], [py[depot], py[a_route[0]]], color=cmap(k))
         plt.plot([px[a_route[-1]], px[depot]], [py[a_route[-1]], py[depot]], color=cmap(k))
 
         # Draw the route: one by one
-        for i in range(0, len(a_route)-1):
+        for i in range(len(a_route)-1):
             plt.plot([px[a_route[i]], px[a_route[i + 1]]], [py[a_route[i]], py[a_route[i + 1]]], color=cmap(k))
             plt.plot(px[a_route[i]], py[a_route[i]], 'co', markersize=5, color=cmap(k))
+            plt.text(px[a_route[i]], py[a_route[i]], str(a_route[i]), fontsize=12, ha='right')
 
         plt.plot(px[a_route[-1]], py[a_route[-1]], 'co', markersize=5, color=cmap(k))
+        plt.text(px[a_route[-1]], py[a_route[-1]], str(a_route[-1]), fontsize=12, ha='right')
 
     plt.title(title)
     plt.show()
